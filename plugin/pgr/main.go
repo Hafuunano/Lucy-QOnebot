5e457850573bbc79d6648643601259a4a906bc46
moyoez <i@himoyo.cn>
2023-06-04 22:11:55 +0800

    remove wife qq

--- plugin/pgr/main.go
+++ plugin/pgr/main.go
@@ -2,6 +2,8 @@ package pgr // Package pgr hosted by Phigros-Library
 
 // too lazy,so this way is to use thrift host server (Working on HiMoYo Cloud.)
 
+// update: use PhigrosUnlimitedAPI + Phigros Library as Maintainer.
+
 func init() {
 
 }
