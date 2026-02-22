65f195ae70ae2c94a74d06210af6182ac3d50aca
MoeMagicMango <i@lemonkoi.one>
2026-02-23 01:10:58 +0800

    chore: add new plugins for echo and hello to the core package

--- core/plugins.go
+++ core/plugins.go
@@ -2,5 +2,7 @@
 package core
 
 import (
+	_ "github.com/Hafuunano/Plugin-Collections/plugins/plugin-echo"
+	_ "github.com/Hafuunano/Plugin-Collections/plugins/plugin-hello"
 	_ "github.com/Hafuunano/Plugin-Collections/plugins/plugin-ping"
 )
